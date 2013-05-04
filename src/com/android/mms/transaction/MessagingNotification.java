44f5a38bd9e20c29263bbf294b57c103c78c94d4
drcmda <drcmda@gmail.com>
2013-05-04 22:35:45 +0100

    taskon home
    Change-Id: Ib628e06fbd1575bd627f35a70ff6c47c71aaecec

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -907,6 +907,7 @@ public class MessagingNotification {
             }
         }
         TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+        taskStackBuilder.setTaskOnHome(false);
 
         // If we have more than one unique thread, change the title (which would
         // normally be the contact who sent the message) to a generic one that
@@ -1308,6 +1309,7 @@ public class MessagingNotification {
         }
 
         TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+        taskStackBuilder.setTaskOnHome(false);
         if (allFailedInSameThread) {
             failedIntent = new Intent(context, ComposeMessageActivity.class);
             if (isDownload) {
