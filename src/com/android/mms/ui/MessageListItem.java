90fb9d4457ef6729d5cab6d6ebcc1578dc17584d
Jorge Ruesga <jorge@ruesga.com>
2014-09-04 01:22:25 +0200

    mms: prevent FC if there isn't and associated activity
    If the activity associated to span (email or browser) is not present or is disabled, then
    that leads to an ActivityNotFoundException. Just graful display a message instead of crash.
    
    JIRA: BUGDUMP-23165
    https://jira.cyanogenmod.org/browse/BUGDUMP-23165
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Change-Id: Iaab2c2aac199f641a6df12e20bda7fecce8c2d39

--- src/com/android/mms/ui/MessageListItem.java
+++ src/com/android/mms/ui/MessageListItem.java
@@ -22,6 +22,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -61,6 +62,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.mms.MmsApp;
 import com.android.mms.R;
@@ -685,7 +687,12 @@ public class MessageListItem extends LinearLayout implements
         if (spans.length == 0) {
             sendMessage(mMessageItem, MSG_LIST_DETAILS);    // show the message details dialog
         } else if (spans.length == 1) {
-            spans[0].onClick(mBodyTextView);
+            try {
+                spans[0].onClick(mBodyTextView);
+            } catch (ActivityNotFoundException ex) {
+                Toast.makeText(mContext, R.string.failed_open_associated_activity_msg,
+                        Toast.LENGTH_SHORT).show();
+            }
         } else {
             ArrayAdapter<URLSpan> adapter =
                 new ArrayAdapter<URLSpan>(mContext, android.R.layout.select_dialog_item, spans) {
@@ -729,7 +736,12 @@ public class MessageListItem extends LinearLayout implements
                 @Override
                 public final void onClick(DialogInterface dialog, int which) {
                     if (which >= 0) {
-                        spans[which].onClick(mBodyTextView);
+                        try {
+                            spans[which].onClick(mBodyTextView);
+                        } catch (ActivityNotFoundException ex) {
+                            Toast.makeText(mContext, R.string.failed_open_associated_activity_msg,
+                                    Toast.LENGTH_SHORT).show();
+                        }
                     }
                     dialog.dismiss();
                 }
